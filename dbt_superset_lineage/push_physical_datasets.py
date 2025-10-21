1b32027ac7b5c50e205da0bb4bce63a08b86fc41
Maksym Loktionov <maksymloktionov@mloktionov-mbp.local>
2025-10-21 14:08:54 +0300

    Remove printing

--- dbt_superset_lineage/push_physical_datasets.py
+++ dbt_superset_lineage/push_physical_datasets.py
@@ -260,7 +260,6 @@ def add_wall_time_columns(dataset):
     wt_columns = []
     existing_column_names = {col['column_name'] for col in dataset['columns_new']}
 
-    print(dataset)
     for col in dataset['columns_new']:
         if col.get('type') == 'TIMESTAMP_TZ':
             original_col_name = col['column_name']
